2b62ea5e3dd890a54b44d15003f4c6d37ed4340a
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-06-20 18:11:29 +0200

    Update package.d

--- source/models/pim/entities/package.d
+++ source/models/pim/entities/package.d
@@ -1,1 +1,9 @@
 module models.pim.entities;
+
+public import models.pim.entities.billofmaterials;
+public import models.pim.entities.ecoressources;
+public import models.pim.entities.pcproducts;
+public import models.pim.entities.pds;
+public import models.pim.entities.pmfformulas;
+public import models.pim.entities.procproducts;
+public import models.pim.entities.unitofmeasures;
