354ddd1f09d8daeacf162f82489d2906c8901952
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-06-20 19:46:33 +0200

    Update package.d

--- source/models/pim/entities/pds/regionals/restricted/package.d
+++ source/models/pim/entities/pds/regionals/restricted/package.d
@@ -1,1 +1,4 @@
 module models.pim.entities.pds.regionals.restricted;
+
+public import models.pim.entities.pds.regionals.restricted.product;
+public import models.pim.entities.pds.regionals.restricted.productlist;
