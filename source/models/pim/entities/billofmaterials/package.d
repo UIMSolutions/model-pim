8596575488d2f2f08c1b255fda6699d15cd7d62f
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-06-20 18:14:38 +0200

    Update package.d

--- source/models/pim/entities/billofmaterials/package.d
+++ source/models/pim/entities/billofmaterials/package.d
@@ -1,1 +1,9 @@
 module models.pim.entities.billofmaterials;
+
+public import models.pim.entities.billofmaterials.versions;
+public import models.pim.entities.billofmaterials.configurationgroup;
+public import models.pim.entities.billofmaterials.header;
+public import models.pim.entities.billofmaterials.itemspecificheader;
+public import models.pim.entities.billofmaterials.line;
+public import models.pim.entities.billofmaterials.parameter;
+
