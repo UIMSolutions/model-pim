1bd5543fcddaee033f9e55274c5cbebdfafafece
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-06-20 20:02:03 +0200

    Update and rename attriibuteassignment.d to attributeassignment.d

---
+++ source/models/pim/entities/ecoressources/products/attributes/groups/attributeassignment.d
@@ -0,0 +1,4 @@
+module models.pim.entities.ecoressources.products.attributes.groups.attributeassignment;
+
+@safe:
+import models.pim;
--- source/models/pim/entities/ecoressources/products/attributes/groups/attriibuteassignment.d
+++
@@ -1,4 +0,0 @@
-module models.pim.entities.ecoressources.products.attributes.groups.attriibuteassignment;
-
-@safe:
-import models.pim;
