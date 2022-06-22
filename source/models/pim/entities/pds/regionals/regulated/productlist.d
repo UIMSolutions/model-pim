f59085b9927f02f9b595af0e2b87ba0d9b2a08a6
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-06-22 20:48:50 +0200

    Update productlist.d

--- source/models/pim/entities/pds/regionals/regulated/productlist.d
+++ source/models/pim/entities/pds/regionals/regulated/productlist.d
@@ -2,3 +2,17 @@ module models.pim.entities.pds.regionals.regulated.productlist;
 
 @safe:
 import models.pim;
+
+class DPDSRegionalsRegulatedProductListEntity : DOOPEntity {
+  mixin(EntityThis!("PDSRegionalsRegulatedProductListEntity"));
+  
+  override void initialize() {
+    super.initialize;
+
+    this
+      .addValues([
+      ])
+      .registerPath("pim_pds.regionals.regulated.productlists");
+  }
+}
+mixin(EntityCalls!("PDSRegionalsRegulatedProductListEntity"));
