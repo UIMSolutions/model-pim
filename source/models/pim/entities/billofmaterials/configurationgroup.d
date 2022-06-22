74e6b5b54a6b371b6a881fbce990649e80fb3a4e
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-06-22 20:33:49 +0200

    Update configurationgroup.d

--- source/models/pim/entities/billofmaterials/configurationgroup.d
+++ source/models/pim/entities/billofmaterials/configurationgroup.d
@@ -1,1 +1,18 @@
 module models.pim.entities.billofmaterials.configurationgroup;
+
+@safe:
+import models.pim;
+
+class DBOMConfigurationGroupEntity : DOOPEntity {
+  mixin(EntityThis!("BOMConfigurationGroupEntity"));
+  
+  override void initialize() {
+    super.initialize;
+
+    this
+      .addValues([
+      ])
+      .registerPath("pim_billofmaterials.configuration.groups");
+  }
+}
+mixin(EntityCalls!("BOMConfigurationGroupEntity"))
