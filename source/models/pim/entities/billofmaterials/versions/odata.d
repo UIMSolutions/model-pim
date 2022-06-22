c6f4b05adbda8239814601c9e8e7766e2897f9f4
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-06-22 20:40:09 +0200

    Update odata.d

--- source/models/pim/entities/billofmaterials/versions/odata.d
+++ source/models/pim/entities/billofmaterials/versions/odata.d
@@ -1,1 +1,18 @@
 module models.pim.entities.billofmaterials.versions.odata;
+
+@safe:
+import models.pim;
+
+class DBOMVersionODataEntity : DOOPEntity {
+  mixin(EntityThis!("BOMVersionODataEntity"));
+  
+  override void initialize() {
+    super.initialize;
+
+    this
+      .addValues([
+      ])
+      .registerPath("pim_billofmaterials.versions.odata");
+  }
+}
+mixin(EntityCalls!("BOMVersionODataEntity"))
