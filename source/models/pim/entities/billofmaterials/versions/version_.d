5e37e10829411d8e81695a726e1e57f69e818992
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-06-22 20:38:54 +0200

    Update version_.d

--- source/models/pim/entities/billofmaterials/versions/version_.d
+++ source/models/pim/entities/billofmaterials/versions/version_.d
@@ -1,2 +1,18 @@
 module models.pim.entities.billofmaterials.versions.version_;
 
+@safe:
+import models.pim;
+
+class DBOMVersionEntity : DOOPEntity {
+  mixin(EntityThis!("BOMVersionEntity"));
+  
+  override void initialize() {
+    super.initialize;
+
+    this
+      .addValues([
+      ])
+      .registerPath("pim_billofmaterials.versions");
+  }
+}
+mixin(EntityCalls!("BOMVersionEntity"))
