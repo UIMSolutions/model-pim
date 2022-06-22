df2cf6afe8f0c02ca073c8a491e08bba2df3e95e
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-06-22 20:34:43 +0200

    Update header.d

--- source/models/pim/entities/billofmaterials/header.d
+++ source/models/pim/entities/billofmaterials/header.d
@@ -1,1 +1,18 @@
 module models.pim.entities.billofmaterials.header;
+
+@safe:
+import models.pim;
+
+class DBOMHeaderEntity : DOOPEntity {
+  mixin(EntityThis!("BOMHeaderEntity"));
+  
+  override void initialize() {
+    super.initialize;
+
+    this
+      .addValues([
+      ])
+      .registerPath("pim_billofmaterials.headers");
+  }
+}
+mixin(EntityCalls!("BOMHeaderEntity"))
