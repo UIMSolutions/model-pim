b9eed30a61acc27e697e503a24ce1699eff51faf
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-06-22 20:43:16 +0200

    Update attribute.d

--- source/models/pim/entities/pds/itembatches/attributes/attribute.d
+++ source/models/pim/entities/pds/itembatches/attributes/attribute.d
@@ -2,3 +2,17 @@ module models.pim.entities.pds.itembatches.attributes.attribute;
 
 @safe:
 import models.pim;
+
+class DPDSItembatchesAttributeEntity : DOOPEntity {
+  mixin(EntityThis!("BOMPDSItembatchesAttributeEntity"));
+  
+  override void initialize() {
+    super.initialize;
+
+    this
+      .addValues([
+      ])
+      .registerPath("pim_pds.itembatches.attributes");
+  }
+}
+mixin(EntityCalls!("PDSItembatchesAttributeEntity"))
