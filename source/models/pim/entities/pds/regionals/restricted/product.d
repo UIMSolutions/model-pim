e77861e6a9434014a8945e7654471aff149782f2
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-06-22 20:50:40 +0200

    Update product.d

--- source/models/pim/entities/pds/regionals/restricted/product.d
+++ source/models/pim/entities/pds/regionals/restricted/product.d
@@ -1,1 +1,18 @@
 module models.pim.entities.pds.regionals.restricted.product;
+
+@safe:
+import models.pim;
+
+class DPDSRegionalsRestrictedProductEntity : DOOPEntity {
+  mixin(EntityThis!("PDSRegionalsRestrictedProductEntity"));
+  
+  override void initialize() {
+    super.initialize;
+
+    this
+      .addValues([
+      ])
+      .registerPath("pim_pds.regionals.restricted.products");
+  }
+}
+mixin(EntityCalls!("PDSRegionalsRestrictedProductEntity"));
