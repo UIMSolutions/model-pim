6d0a937d6bac25df0347963b5473e843543b79f7
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-06-22 20:35:47 +0200

    Update parameter.d

--- source/models/pim/entities/billofmaterials/parameter.d
+++ source/models/pim/entities/billofmaterials/parameter.d
@@ -1,1 +1,18 @@
 module models.pim.entities.billofmaterials.parameter.d
+
+@safe:
+import models.pim;
+
+class DBOMParameterEntity : DOOPEntity {
+  mixin(EntityThis!("BOMParameterEntity"));
+  
+  override void initialize() {
+    super.initialize;
+
+    this
+      .addValues([
+      ])
+      .registerPath("pim_billofmaterials.parameters");
+  }
+}
+mixin(EntityCalls!("BOMParameterEntity"))
