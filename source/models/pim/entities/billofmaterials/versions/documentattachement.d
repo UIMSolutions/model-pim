9a07c994341756db10ce160a1e65d300f2c631e2
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-06-22 20:41:27 +0200

    Update documentattachement.d

--- source/models/pim/entities/billofmaterials/versions/documentattachement.d
+++ source/models/pim/entities/billofmaterials/versions/documentattachement.d
@@ -1,2 +1,18 @@
 module models.pim.entities.billofmaterials.versions.documentattachement;
 
+@safe:
+import models.pim;
+
+class DBOMVersionDocumentAttachementEntity : DOOPEntity {
+  mixin(EntityThis!("BOMVersionDocumentAttachementEntity"));
+  
+  override void initialize() {
+    super.initialize;
+
+    this
+      .addValues([
+      ])
+      .registerPath("pim_billofmaterials.versions.documentattachements");
+  }
+}
+mixin(EntityCalls!("BOMVersionDocumentAttachementEntity"))
