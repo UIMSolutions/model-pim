f261915f81958fa5b5da0f9a7ed23ac5b69b6ec9
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-06-22 20:36:36 +0200

    Update line.d

--- source/models/pim/entities/billofmaterials/line.d
+++ source/models/pim/entities/billofmaterials/line.d
@@ -1,1 +1,18 @@
 module models.pim.entities.billofmaterials.line;
+
+@safe:
+import models.pim;
+
+class DBOMLineEntity : DOOPEntity {
+  mixin(EntityThis!("BOMLineEntity"));
+  
+  override void initialize() {
+    super.initialize;
+
+    this
+      .addValues([
+      ])
+      .registerPath("pim_billofmaterials.line");
+  }
+}
+mixin(EntityCalls!("BOMLineEntity"))
