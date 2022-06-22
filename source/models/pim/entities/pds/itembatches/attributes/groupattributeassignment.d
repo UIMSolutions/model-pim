52dd22c946ea19589f2f82ca405bcfbd6eb19650
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-06-22 20:45:30 +0200

    Update groupattributeassignment.d

--- source/models/pim/entities/pds/itembatches/attributes/groupattributeassignment.d
+++ source/models/pim/entities/pds/itembatches/attributes/groupattributeassignment.d
@@ -2,3 +2,17 @@ module models.pim.entities.pds.itembatches.attributes.groupattributeassignment;
 
 @safe:
 import models.pim;
+
+class DPDSItembatchesAttributesGroupAttributeAssignmentEntity : DOOPEntity {
+  mixin(EntityThis!("PDSItembatchesAttributesGroupAttributeAssignmentEntity"));
+  
+  override void initialize() {
+    super.initialize;
+
+    this
+      .addValues([
+      ])
+      .registerPath("pim_pds.itembatches.attributes.groupattribute.assignments");
+  }
+}
+mixin(EntityCalls!("PDSItembatchesAttributesGroupAttributeAssignmentEntity"));
